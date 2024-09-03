1cd3819087bff4b7989f1fa9da46852f257bad7f
Pablox2009 <pabloliquitaya2017@gmail.com>
2024-09-03 15:18:24 -0300

    Nuevas pantallas

--- Health/app/src/main/java/com/example/health/menu_principal.java
+++ Health/app/src/main/java/com/example/health/menu_principal.java
@@ -10,25 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class menu_principal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu_principal);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void menu_principal (View view) {
-        Intent m=new Intent(this, menu_principal.class );
-        startActivity(m);
-    }
-   public void registrar(View view){
-        Intent r = new Intent(this,registrar.class);
-        startActivity(r);
-   }
+
 }
